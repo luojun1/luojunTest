9a8e221338409a10b76ee1819b800d20553914de
luojun1 <luojun.wanghengyan@aliyun.com>
2019-01-10 18:27:16 +0800

    block test

--- CustomTest/CustomTest/ViewController.m
+++ CustomTest/CustomTest/ViewController.m
@@ -8,16 +8,49 @@
 
 #import "ViewController.h"
 
+
+typedef int (^blk_t)(void);
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
 
+-(void) globalBlock {
+    __block int a = 10;
+    blk_t block = ^{
+        
+        NSLog(@"block test %d",a);
+        return 1;
+    };
+//    block();
+    
+//    __block int val =10;
+//    blkt1 s= ^{
+//        return ++val;};
+//    s();
+//    blkt1 h = [s copy];
+//    h();
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self globalBlock];
+    
+    [self foo];
 }
 
+-(void) foo
+{
+    __block int i = 1024;
+    int j = 1;
+    void (^blk)(void);
+//    void (^blkInHeap)(void);
+    blk = ^{ printf("1111\n");};//blk在栈里
+//    blkInHeap = Block_copy(blk);//blkInHeap在堆里
+}
 
 @end
--- CustomTest/CustomTest/blockTest.m
+++ CustomTest/CustomTest/blockTest.m
@@ -10,18 +10,59 @@
 
 typedef void (^BlockType)(void);
 
+typedef int (^BlockTest) (int,int);
+
+/// 数组元素转换
+typedef id(^LXDItemMap)(id item);   //  这里block完成对元素的操作
+typedef NSArray *(^LXDArrayMap)(LXDItemMap itemMap);
+
+/// 数组元素筛选
+typedef BOOL(^LXDItemFilter)(id item);
+typedef NSArray *(^LXDArrayFilter)(LXDItemFilter itemFilter);
+
+
 @interface blockTest ()
 
 @property (nonatomic, copy) BlockType block;
+
+@property (nonatomic, copy, readonly) LXDArrayMap map;
+@property (nonatomic, copy, readonly) LXDArrayFilter filter;
 
 @end
 
 @implementation blockTest
 
+
+
+- (void)setFilter:(LXDArrayFilter)filter {}
+- (void)setMap:(LXDArrayMap)map {}
+
+
+
+
 - (void) FIrstTest {
+//#pragma mark - 筛选数组中大于20的数值并转换成字符串
+//    NSArray<NSNumber *> * numbers = @[@10, @15, @99, @66, @25, @28.1, @7.5, @11.2, @66.2];
+//    NSArray * result = numbers.filter(^BOOL(NSNumber * item) {
+//        return item.doubleValue > 20
+//    }).map(^id(NSNumber * item) {
+//        return [NSString stringWithFormat: @"string %g", item.doubleValue];
+//    });
+//
+//#pragma mark - 将数组中的字典转换成对应的数据模型
+//    NSArray<NSDictionary *> * jsons = @[@{ ... }, @{ ... }, @{ ... }];
+//    NSArray<LXDModel *> * models = jsons.map(^id(id item) {
+//        return [[LXDModel alloc] initWithJSON: item];
+//    })
+    
     self.block = ^(){
         NSLog(@"这是一个block");
     };
+}
+
+- (int) testbBlockT:(BlockTest)blockBB {
+    
+    return 1;
 }
 
 - (int) parameterOne:(int )a parameterTwo:(int) b blockA: (int (^)(int a, int b))blockC {
